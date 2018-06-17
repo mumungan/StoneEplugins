953fa85a15a6fb78617046e8c50da69ad731dfe0
Stone <mumungan@gmail.com>
2018-06-17 21:13:47 +0900

    18.06.17

--- BossCCDebuffplugin.cs
+++ BossCCDebuffplugin.cs
@@ -9,10 +9,12 @@ namespace Turbo.Plugins.Stone
     {
         public WorldDecoratorCollection BossCCDecorator { get; set; }
         public WorldDecoratorCollection BossDebuffDecorator { get; set; }
+        public WorldDecoratorCollection BossDirectionLineDecorator { get; set; }
         public IFont BossCCDebuffFont { get; set; }
         public bool showCC { get; set; }
         public bool showDebuff { get; set; }
         public bool showCCoffMessage { get; set; }
+        public bool showDirectionLine { get; set; }
         public float CCoffStarttick { get; set; }
         private bool CCofftimerRunning = false;
 
@@ -27,19 +29,34 @@ namespace Turbo.Plugins.Stone
             showCC = true;
             showDebuff = true;
             showCCoffMessage = true;
+            showDirectionLine = true;
             BossCCDecorator = new WorldDecoratorCollection(
-                new GroundLabelDecorator(Hud) 
-		    {
+                new GroundLabelDecorator(Hud)
+                {
                     BackgroundBrush = Hud.Render.CreateBrush(255, 255, 255, 0, 0),
                     BorderBrush = Hud.Render.CreateBrush(255, 112, 48, 160, -1),
                     TextFont = Hud.Render.CreateFont("tahoma", 10f, 255, 112, 48, 160, true, false, 255, 0, 0, 0, true),
-            });
+                });
             BossDebuffDecorator = new WorldDecoratorCollection(
             new GroundLabelDecorator(Hud)
             {
-                    BackgroundBrush = Hud.Render.CreateBrush(255, 255, 128, 0, 0),
-                    BorderBrush = Hud.Render.CreateBrush(255, 112, 48, 160, -1),
-                    TextFont = Hud.Render.CreateFont("tahoma", 10f, 255, 112, 48, 160, true, false, 255, 0, 0, 0, true),
+                BackgroundBrush = Hud.Render.CreateBrush(255, 255, 128, 0, 0),
+                BorderBrush = Hud.Render.CreateBrush(255, 112, 48, 160, -1),
+                TextFont = Hud.Render.CreateFont("tahoma", 10f, 255, 112, 48, 160, true, false, 255, 0, 0, 0, true),
+            });
+            BossDirectionLineDecorator = new WorldDecoratorCollection(
+            new MapShapeDecorator(Hud)
+            {
+                Brush = Hud.Render.CreateBrush(192, 255, 255, 55, -1),
+                ShadowBrush = Hud.Render.CreateBrush(96, 0, 0, 0, 1),
+                Radius = 10.0f,
+                ShapePainter = new LineFromMeShapePainter(Hud),
+            },
+            new MapLabelDecorator(Hud)
+            {
+                LabelFont = Hud.Render.CreateFont("tahoma", 6f, 200, 255, 255, 0, false, false, 128, 0, 0, 0, true),
+                RadiusOffset = 10,
+                Up = true,
             });
         }
 
@@ -49,6 +66,11 @@ namespace Turbo.Plugins.Stone
             foreach (var monster in monsters)
                 if (monster.Rarity == ActorRarity.Boss)
                 {
+                    if (showDirectionLine)
+                    {
+                        BossDirectionLineDecorator.ToggleDecorators<GroundLabelDecorator>(!monster.FloorCoordinate.IsOnScreen()); // do not display ground labels when the marker is on the screen
+                        BossDirectionLineDecorator.Paint(layer, null, monster.FloorCoordinate, null);
+                    }
                     if (showCCoffMessage && !monster.Frozen && !monster.Chilled && !monster.Slow && !monster.Stunned && !monster.Blind)
                     {
                         var CCofftime = (Hud.Game.CurrentGameTick - CCoffStarttick) / 60.0d;
---
+++ WizCOEBuffSwich.cs
@@ -0,0 +1,140 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using Turbo.Plugins.Default;
+
+namespace Turbo.Plugins.Stone
+{
+    public class WizCOEBuffSwich : BasePlugin, IInGameTopPainter
+    {
+
+        public bool HideWhenUiIsHidden { get; set; }
+        public BuffPainter BuffPainter { get; set; }
+
+        private BuffRuleCalculator _ruleCalculator;
+
+        public WizCOEBuffSwich()
+        {
+            Enabled = true;
+        }
+
+        public override void Load(IController hud)
+        {
+            base.Load(hud);
+
+            HideWhenUiIsHidden = false;
+            BuffPainter = new BuffPainter(Hud, true)
+            {
+                Opacity = 1.0f,
+                TimeLeftFont = Hud.Render.CreateFont("tahoma", 15, 255, 255, 255, 255, true, false, 255, 0, 0, 0, true),
+            };
+
+            _ruleCalculator = new BuffRuleCalculator(Hud);
+            _ruleCalculator.SizeMultiplier = 0.75f;
+
+            _ruleCalculator.Rules.Add(new BuffRule(430674) { IconIndex = 1, MinimumIconCount = 0, DisableName = true }); // Arcane
+            _ruleCalculator.Rules.Add(new BuffRule(430674) { IconIndex = 2, MinimumIconCount = 0, DisableName = true }); // Cold
+            _ruleCalculator.Rules.Add(new BuffRule(430674) { IconIndex = 3, MinimumIconCount = 0, DisableName = true }); // Fire
+            _ruleCalculator.Rules.Add(new BuffRule(430674) { IconIndex = 4, MinimumIconCount = 0, DisableName = true }); // Holy
+            _ruleCalculator.Rules.Add(new BuffRule(430674) { IconIndex = 5, MinimumIconCount = 0, DisableName = true }); // Lightning
+            _ruleCalculator.Rules.Add(new BuffRule(430674) { IconIndex = 6, MinimumIconCount = 0, DisableName = true }); // Physical
+            _ruleCalculator.Rules.Add(new BuffRule(430674) { IconIndex = 7, MinimumIconCount = 0, DisableName = true }); // Poison
+
+        }
+
+        private IEnumerable<BuffRule> GetCurrentRules(HeroClass heroClass)
+        {
+            for (int i = 1; i <= 7; i++)
+            {
+                switch (heroClass)
+                {
+                    case HeroClass.Barbarian: if (i == 1 || i == 4 || i == 7) continue; break;
+                    case HeroClass.Crusader: if (i == 1 || i == 2 || i == 7) continue; break;
+                    case HeroClass.DemonHunter: if (i == 1 || i == 4 || i == 7) continue; break;
+                    case HeroClass.Monk: if (i == 1 || i == 7) continue; break;
+                    case HeroClass.Necromancer: if (i == 1 || i == 3 || i == 4 || i == 5) continue; break;
+                    case HeroClass.WitchDoctor: if (i == 1 || i == 4 || i == 5) continue; break;
+                    case HeroClass.Wizard: if (i == 4 || i == 6 || i == 7) continue; break;
+                }
+                yield return _ruleCalculator.Rules[i - 1];
+            }
+        }
+
+        public void PaintTopInGame(ClipState clipState)
+        {
+            if (clipState != ClipState.BeforeClip) return;
+            if (HideWhenUiIsHidden && Hud.Render.UiHidden) return;
+            if (Hud.Game.Me.HeroClassDefinition.HeroClass != HeroClass.Wizard) return;
+            foreach (var player in Hud.Game.Players)
+            {
+                if (player.IsMe)
+                {
+                    if (!player.HasValidActor) continue;
+
+
+                    var buff = player.Powers.GetBuff(430674);
+                    if ((buff == null) || (buff.IconCounts[0] <= 0)) continue;
+
+                    var classSpecificRules = GetCurrentRules(player.HeroClassDefinition.HeroClass);
+
+                    _ruleCalculator.CalculatePaintInfo(player, classSpecificRules);
+
+                    if (_ruleCalculator.PaintInfoList.Count == 0) return;
+                    if (!_ruleCalculator.PaintInfoList.Any(info => info.TimeLeft > 0)) return;
+
+                    var highestElementalBonus = player.Offense.HighestElementalDamageBonus;
+
+                    for (int i = 0; i < _ruleCalculator.PaintInfoList.Count; i++)
+                    {
+                        var info = _ruleCalculator.PaintInfoList[0];
+                        if (info.TimeLeft <= 0)
+                        {
+                            _ruleCalculator.PaintInfoList.RemoveAt(0);
+                            _ruleCalculator.PaintInfoList.Add(info);
+                        }
+                        else break;
+                    }
+
+                    for (int orderIndex = 0; orderIndex < _ruleCalculator.PaintInfoList.Count; orderIndex++)
+                    {
+                        var info = _ruleCalculator.PaintInfoList[orderIndex];
+                        var best = false;
+                        switch (info.Rule.IconIndex)
+                        {
+                            case 1: best = player.Offense.BonusToArcane == highestElementalBonus; break;
+                            case 2: best = player.Offense.BonusToCold == highestElementalBonus; break;
+                            case 3: best = player.Offense.BonusToFire == highestElementalBonus; break;
+                            case 4: best = player.Offense.BonusToHoly == highestElementalBonus; break;
+                            case 5: best = player.Offense.BonusToLightning == highestElementalBonus; break;
+                            case 6: best = player.Offense.BonusToPhysical == highestElementalBonus; break;
+                            case 7: best = player.Offense.BonusToPoison == highestElementalBonus; break;
+                        }
+                        if (best) info.Size *= 1.35f;
+                        if (best && orderIndex > 0)
+                        {
+                            var skill = player.Powers.UsedSkills.FirstOrDefault(s => s.SnoPower.Sno == Hud.Sno.SnoPowers.Wizard_BlackHole.Sno);
+                            if (skill != null)
+                                {
+                                    info.TimeLeft = (orderIndex - 1) * 4 - 3 + _ruleCalculator.PaintInfoList[0].TimeLeft;
+                                }
+                                else
+                                {
+                                    info.TimeLeft = (orderIndex - 1) * 4 - 1 + _ruleCalculator.PaintInfoList[0].TimeLeft;
+                                }
+                        }
+                        else info.TimeLeftNumbersOverride = false;
+                    }
+
+                    var portraitRect = player.PortraitUiElement.Rectangle;
+
+                    var x = portraitRect.Right * 8.2f;
+                    var y = portraitRect.Top + portraitRect.Height * 2.0f;
+
+                    BuffPainter.PaintHorizontal(_ruleCalculator.PaintInfoList, x, y, _ruleCalculator.StandardIconSize, 0);
+                }
+            }
+        }
+
+    }
+
+}
