cae4f1b7aba2759c6e2c0598257575d5cbd0b710
Stone <mumungan@gmail.com>
2018-07-25 14:11:58 +0900

    Delete invalid null

--- StarpactBuffSnapShotPlugin.cs
+++ StarpactBuffSnapShotPlugin.cs
@@ -12,8 +12,8 @@ namespace Turbo.Plugins.Stone
         private IFont StackFont { get; set; }
 		private IFont textFont { get; set; }
 		private IBrush visionBrush, edgeBrush, dynamoBrush;
-		private int blackHolesb, blackHole, waveOfForcesb, waveOfForce, arcaneDynamosb, arcaneDynamo;
-		private float resourcesb, resource;
+		private int blackHolesb = 0, blackHole = 0, waveOfForcesb = 0, waveOfForce = 0, arcaneDynamosb = 0, arcaneDynamo = 0;
+		private float resourcesb = 0, resource = 0;
 		private bool getv, getv1, getv2;
 		private string coe;
 
@@ -26,8 +26,8 @@ namespace Turbo.Plugins.Stone
         {
             base.Load(hud);
   
-			textFont = Hud.Render.CreateFont("tahoma", 14, 255, 255, 255, 255, false, false, 255, 0, 0, 0, true);
-            StackFont = Hud.Render.CreateFont("tahoma", 15, 255, 255, 255, 255, false, false, 255, 0, 0, 0, true);
+			textFont = Hud.Render.CreateFont("tahoma", 16, 255, 255, 255, 255, false, false, 255, 0, 0, 0, true);
+            StackFont = Hud.Render.CreateFont("tahoma", 18, 255, 255, 255, 255, false, false, 255, 0, 0, 0, true);
 			visionBrush = Hud.Render.CreateBrush(255, 112, 48, 160, 0);
 			edgeBrush = Hud.Render.CreateBrush(255, 255, 255, 255, -2);
 			dynamoBrush = Hud.Render.CreateBrush(150, 150, 150, 150, 0);
@@ -37,12 +37,11 @@ namespace Turbo.Plugins.Stone
         {
             var actors = Hud.Game.Actors;
             var me = Hud.Game.Me;
-			if (Hud.Game.Me.HeroClassDefinition.HeroClass != HeroClass.Wizard) return;
             sbremaining = 1.25f - ((Hud.Game.CurrentGameTick - sbstarpactstarttict) / 60.0f);
 			if (sbstarpacttimerRunning == true && sbremaining <= 0) sbstarpacttimerRunning = false;
             if (sbremaining < 0) sbremaining = 0;  	
-            float x = Hud.Window.Size.Width / 2 - Hud.Window.Size.Width * 0.042f;
-            float y  = Hud.Window.Size.Height / 2 - Hud.Window.Size.Height * 0.41f;
+            float x = Hud.Window.Size.Width / 2 - Hud.Window.Size.Width * 0.038f;
+            float y  = Hud.Window.Size.Height / 2 - Hud.Window.Size.Height * 0.4f;
             var rect = new RectangleF(x, y, 40.0f, 40.0f);
 			if (coe == "V")
 			{
@@ -51,22 +50,18 @@ namespace Turbo.Plugins.Stone
 			}
 			dynamoBrush.DrawRectangle(rect.X + 120.0f, rect.Y, 40.0f, 40.0f);
 
-			if (resourcesb == null) resourcesb = 0;
 			var resourcetext = textFont.GetTextLayout(Math.Truncate(resourcesb).ToString());
 			textFont.DrawText(resourcetext, Hud.Window.Size.Width * 0.435f, Hud.Window.Size.Height * 0.1f);
 			Hud.Texture.GetItemTexture(Hud.Sno.SnoItems.P2_Unique_Ring_04).Draw(rect);
 			if (String.IsNullOrEmpty(coe)) coe = "";
 			var coetext = StackFont.GetTextLayout(coe);
 			StackFont.DrawText(coetext, rect.Right - (rect.Width / 8.0f) - (float)Math.Ceiling(coetext.Metrics.Width), rect.Bottom - coetext.Metrics.Height);
-			if (blackHolesb == null) blackHolesb = 0;
 			Hud.Texture.GetTexture(Hud.Sno.GetSnoPower(243141).NormalIconTextureId).Draw(rect.X + 40.0f, rect.Y, 40.0f, 40.0f);	
 			var layout = StackFont.GetTextLayout(blackHolesb.ToString());
 			StackFont.DrawText(layout, rect.Right - (rect.Width / 8.0f) - (float)Math.Ceiling(layout.Metrics.Width) + 40.0f, rect.Bottom - layout.Metrics.Height);
-			if (waveOfForcesb == null) waveOfForcesb = 0;
 			Hud.Texture.GetTexture(Hud.Sno.GetSnoPower(30796).NormalIconTextureId).Draw(rect.X + 80.0f, rect.Y, 40.0f, 40.0f);	
 			var layout1 = StackFont.GetTextLayout(waveOfForcesb.ToString());
 			StackFont.DrawText(layout1, rect.Right - (rect.Width / 8.0f) - (float)Math.Ceiling(layout1.Metrics.Width) + 80.0f, rect.Bottom - layout1.Metrics.Height);
-			if (arcaneDynamosb == null) arcaneDynamosb = 0;
 			Hud.Texture.GetTexture(Hud.Sno.GetSnoPower(208823).NormalIconTextureId).Draw(rect.X + 120.0f, rect.Y, 40.0f, 40.0f);	
 			var layout2 = StackFont.GetTextLayout(arcaneDynamosb.ToString());
 			StackFont.DrawText(layout2, rect.Right - (rect.Width / 8.0f) - (float)Math.Ceiling(layout2.Metrics.Width) + 120.0f, rect.Bottom - layout2.Metrics.Height);
@@ -127,7 +122,7 @@ namespace Turbo.Plugins.Stone
                         case 217142:
                             if (Hud.Game.Me.HeroClassDefinition.HeroClass == HeroClass.Wizard)
                             {
-                                if (Hud.Game.Me.HeroClassDefinition.HeroClass == HeroClass.Wizard && me.Stats.ResourceCurArcane == 0)
+                                if (Hud.Game.Me.HeroClassDefinition.HeroClass == HeroClass.Wizard && me.Stats.ResourceCurArcane < 5)
                                 {
                                     if (!sbstarpacttimerRunning)
                                     {
@@ -157,7 +152,7 @@ namespace Turbo.Plugins.Stone
 										getv1 = false;
 										getv2 = false;
                                     }
-	                                if ( sbremaining < 0.4 && sbremaining > 0)
+	                                if ( sbremaining < 0.1 && sbremaining > 0)
 									{
 										if (me.Powers.BuffIsActive(430674, 1)) coe = "V";
 										else if (me.Powers.BuffIsActive(430674, 2)) coe = "C";
